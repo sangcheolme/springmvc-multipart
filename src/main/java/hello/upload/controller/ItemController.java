7ba01a504eaada6a8e367f08da1e773dfcb97088
SangCheol <scpark0698@gmail.com>
2023-08-22 23:24:02 +0900

    upload 예제 - 다운로드

---
+++ src/main/java/hello/upload/controller/ItemController.java
@@ -0,0 +1,88 @@
+package hello.upload.controller;
+
+import hello.upload.domain.Item;
+import hello.upload.domain.ItemRepository;
+import hello.upload.domain.UploadFile;
+import hello.upload.file.FileStore;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.buf.UriUtil;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriUtils;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemRepository itemRepository;
+    private final FileStore fileStore;
+
+    @GetMapping("/items/new")
+    public String newItem(@ModelAttribute ItemForm form) {
+        return "item-form";
+    }
+
+    @PostMapping("/items/new")
+    public String saveItem(@ModelAttribute ItemForm form,
+                           RedirectAttributes redirectAttributes) throws IOException {
+        UploadFile attachFile = fileStore.storeFile(form.getAttachFile());
+        List<UploadFile> storeImageFiles = fileStore.storeFiles(form.getImageFiles());
+
+        // 데이터베이스에 저장
+        Item item = new Item();
+        item.setItemName(form.getItemName());
+        item.setAttachFile(attachFile);
+        item.setImageFiles(storeImageFiles);
+        itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId", item.getId());
+
+        return "redirect:/items/{itemId}";
+    }
+
+    @GetMapping("/items/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "item-view";
+    }
+
+    @ResponseBody
+    @GetMapping("/images/{fileName}")
+    public Resource downloadImage(@PathVariable String fileName) throws MalformedURLException {
+        // "file:/Users/.../9d670ced-959f-42ce-b93d-24f2dbba16c3.png"
+        return new UrlResource("file:" + fileStore.getFullPath(fileName));
+    }
+
+    @GetMapping("/attach/{itemId}")
+    public ResponseEntity<Resource> downloadAttach(@PathVariable Long itemId) throws MalformedURLException {
+        Item item = itemRepository.findById(itemId);
+        String storeFileName = item.getAttachFile().getStoreFileName();
+        String uploadFileName = item.getAttachFile().getUploadFileName();
+
+        UrlResource resource = new UrlResource("file:" + fileStore.getFullPath(storeFileName));
+
+        log.info("uploadFileName={}", uploadFileName);
+
+        String encodedUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8);
+        String contentDisposition = "attachment; filename=\"" + encodedUploadFileName + "\"";
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(resource);
+    }
+}
